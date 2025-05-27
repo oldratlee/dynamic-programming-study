7bb847c4117920c31c3f83760565baf23b47bdb4
Jerry Lee <oldratlee@gmail.com>
2025-05-27 17:25:40 +0800

    feat(`coin_row`): new implementation `coin_row_eliminate_unused_old_states_space` 🪙

--- tests/test_coin_row.py
+++ tests/test_coin_row.py
@@ -1,18 +1,27 @@
-from dynamic_programming_study.coin_row import coin_row_conventional
+from collections.abc import Callable, Sequence
+
+import pytest
+
+from dynamic_programming_study.coin_row import (
+    coin_row_conventional, coin_row_optimise_states_space,
+)
 
 
-def test_coin_row_conventional():
-    assert coin_row_conventional(()) == 0
-    assert coin_row_conventional([]) == 0
+@pytest.mark.parametrize("impl", [
+    coin_row_conventional, coin_row_optimise_states_space,
+])
+def test_coin_row(impl: Callable[[Sequence[int]], int]):
+    assert impl(tuple()) == 0
+    assert impl([]) == 0
 
-    assert coin_row_conventional((1,)) == 1
-    assert coin_row_conventional([42]) == 42
+    assert impl((1,)) == 1
+    assert impl([42]) == 42
 
-    assert coin_row_conventional([1, 2]) == 2
-    assert coin_row_conventional([2, 1]) == 2
+    assert impl([1, 2]) == 2
+    assert impl([2, 1]) == 2
 
-    assert coin_row_conventional([1, 2, 3]) == 4
-    assert coin_row_conventional([1, 10, 3]) == 10
+    assert impl([1, 2, 3]) == 4
+    assert impl([1, 10, 3]) == 10
 
-    assert coin_row_conventional([1, 2, 3, 10]) == 12
-    assert coin_row_conventional([1, 1, 3, 1]) == 4
+    assert impl([1, 2, 3, 10]) == 12
+    assert impl([1, 1, 3, 1]) == 4
