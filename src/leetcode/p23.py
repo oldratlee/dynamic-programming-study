9914a5fe5327e8b1e954af03775c57dcc605ea0a
Jerry Lee <oldratlee@gmail.com>
2026-08-05 22:49:26 +0800

    feat: implement p23 merge_k_sorted_lists with heap and add second implementation

--- src/leetcode/p23.py
+++ src/leetcode/p23.py
@@ -1,6 +1,6 @@
 """
 23. Merge k Sorted Lists
-https://leetcode.com/problems/merge-k-sorted-lists/description/
+https://leetcode.com/problems/merge-k-sorted-lists
 
 You are given an array of k linked-lists lists,
 each linked-list is sorted in ascending order.
@@ -36,9 +36,42 @@ Constraints:
 * lists[i] is sorted in ascending order.
 * The sum of lists[i].length will not exceed 104.
 """
+import heapq
+from dataclasses import dataclass
 
 
 def merge_k_sorted_lists(lists: list[list[int]]) -> list[int]:
+    heap_list = [_ElementInHeap(lst[0], idx, lst, 0)
+                 for idx, lst in enumerate(lists) if lst]
+    heapq.heapify(heap_list)
+
+    ret: list[int] = []
+    while heap_list:
+        ele = heapq.heappop(heap_list)
+        ret.append(ele.head)
+
+        ele.head_index += 1
+        if ele.head_index == len(ele.lst):
+            continue
+        ele.head = ele.lst[ele.head_index]
+        heapq.heappush(heap_list, ele)
+
+    return ret
+
+
+@dataclass(order=True)
+class _ElementInHeap:
+    # Unique heap ordering key:
+    #  - current head value
+    #  - List index in input; tie-breaker so equal heads stay stably ordered
+    head: int
+    list_index_in_input: int
+    # Payload (not part of the ordering key)
+    lst: list[int]
+    head_index: int
+
+
+def merge_k_sorted_lists_2(lists: list[list[int]]) -> list[int]:
     sorted_lists: list[tuple[list[int], int]] = sorted(
         ((l, idx) for idx, l in enumerate(lists) if l),
         key=lambda x: (x[0][0], x[1])
