291afad09a0d7800f6fb50f9634c1352f467b4d7
Jerry Lee <oldratlee@gmail.com>
2026-08-05 22:49:26 +0800

    feat: implement p23 merge_k_sorted_lists with heap and add second implementation

--- tests/leetcode/test_p23.py
+++ tests/leetcode/test_p23.py
@@ -1,17 +1,23 @@
-from leetcode.p23 import merge_k_sorted_lists
+from collections.abc import Callable
+
+import pytest
+
+from leetcode.p23 import merge_k_sorted_lists, merge_k_sorted_lists_2
 
 
-def test_merge_k_sorted_lists():
-    assert merge_k_sorted_lists([]) == []
-    assert merge_k_sorted_lists([[]]) == []
-    assert merge_k_sorted_lists([[], []]) == []
+@pytest.mark.parametrize("impl", [
+    merge_k_sorted_lists, merge_k_sorted_lists_2])
+def test_merge_k_sorted_lists(impl: Callable[[list[list[int]]], list[int]]):
+    assert impl([]) == []
+    assert impl([[]]) == []
+    assert impl([[], []]) == []
 
-    assert merge_k_sorted_lists([[1, 3]]) == [1, 3]
-    assert merge_k_sorted_lists([[1, 3], []]) == [1, 3]
-    assert merge_k_sorted_lists([[], [1, 3]]) == [1, 3]
+    assert impl([[1, 3]]) == [1, 3]
+    assert impl([[1, 3], []]) == [1, 3]
+    assert impl([[], [1, 3]]) == [1, 3]
 
-    assert merge_k_sorted_lists([[2, 4], [1, 3]]) == [1, 2, 3, 4]
+    assert impl([[2, 4], [1, 3]]) == [1, 2, 3, 4]
 
-    assert merge_k_sorted_lists([
+    assert impl([
         [1, 4, 5], [1, 3, 4], [2, 6]
     ]) == [1, 1, 2, 3, 4, 4, 5, 6]
