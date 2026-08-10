8ecf28760cb3e7e19353a91ee1d5f684af221768
Jerry Lee <oldratlee@gmail.com>
2026-08-11 00:06:48 +0800

    feat: implement p23 merge_k_sorted_linked_lists

--- tests/leetcode/test_p23.py
+++ tests/leetcode/test_p23.py
@@ -2,7 +2,35 @@ from collections.abc import Callable
 
 import pytest
 
-from leetcode.p23 import merge_k_sorted_lists, merge_k_sorted_lists_2
+from leetcode.p23 import (ListNode, merge_k_sorted_linked_lists,
+                          merge_k_sorted_lists, merge_k_sorted_lists_2)
+
+
+def test_merge_k_sorted_linked_lists():
+    assert merge_k_sorted_linked_lists(cc([])) == c([])
+    assert merge_k_sorted_linked_lists(cc([[]])) == c([])
+    assert merge_k_sorted_linked_lists(cc([[], []])) == c([])
+
+    assert merge_k_sorted_linked_lists(cc([[1, 3]])) == c([1, 3])
+    assert merge_k_sorted_linked_lists(cc([[1, 3], []])) == c([1, 3])
+    assert merge_k_sorted_linked_lists(cc([[], [1, 3]])) == c([1, 3])
+
+    assert merge_k_sorted_linked_lists(cc([[2, 4], [1, 3]])) == c([1, 2, 3, 4])
+
+    assert merge_k_sorted_linked_lists(cc([
+        [1, 4, 5], [1, 3, 4], [2, 6]
+    ])) == c([1, 1, 2, 3, 4, 4, 5, 6])
+
+
+def c(lst: list[int]) -> ListNode | None:
+    ret: ListNode | None = None
+    for x in reversed(lst):
+        ret = ListNode(x, ret)
+    return ret
+
+
+def cc(lists: list[list[int]]) -> list[ListNode | None]:
+    return [c(lst) for lst in lists]
 
 
 @pytest.mark.parametrize("impl", [
