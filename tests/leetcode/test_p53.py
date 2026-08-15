1b50e3d84513796d833f59eca18484d912f8365e
Jerry Lee <oldratlee@gmail.com>
2026-08-15 23:52:38 +0800

    feat(p53): add max_nonempty_subarray_sum

--- tests/leetcode/test_p53.py
+++ tests/leetcode/test_p53.py
@@ -1,15 +1,25 @@
-from leetcode.p53 import max_sub_array_sum
+from leetcode.p53 import max_nonempty_subarray_sum, max_subarray_sum
 
 
-def test_max_sub_array_sum():
-    assert max_sub_array_sum([]) == 0
+def test_max_subarray_sum():
+    assert max_subarray_sum([]) == 0
 
-    assert max_sub_array_sum([-1]) == 0
-    assert max_sub_array_sum([-1, -2]) == 0
+    assert max_subarray_sum([-1]) == 0
+    assert max_subarray_sum([-1, -2]) == 0
+    assert max_subarray_sum([-3, -1, -2]) == 0
 
-    assert max_sub_array_sum([1]) == 1
-    assert max_sub_array_sum([5, 4, -1, 7, 8]) == 23
-    assert max_sub_array_sum([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
+    assert max_subarray_sum([1]) == 1
+    assert max_subarray_sum([5, 4, -1, 7, 8]) == 23
+    assert max_subarray_sum([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
 
-    assert max_sub_array_sum([-1]) == 0
-    assert max_sub_array_sum([-1, -3]) == 0
+
+def test_max_nonempty_subarray_sum():
+    assert max_nonempty_subarray_sum([]) is None
+
+    assert max_nonempty_subarray_sum([-1]) == -1
+    assert max_nonempty_subarray_sum([-1, -2]) == -1
+    assert max_nonempty_subarray_sum([-3, -1, -2]) == -1
+
+    assert max_nonempty_subarray_sum([1]) == 1
+    assert max_nonempty_subarray_sum([5, 4, -1, 7, 8]) == 23
+    assert max_nonempty_subarray_sum([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
